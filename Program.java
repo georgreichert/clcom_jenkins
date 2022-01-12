2c2e708647677b4c597eabb23c563b73828b0f9b
georgreichert <81485014+georgreichert@users.noreply.github.com>
2022-01-12 11:57:59 +0100

    Testing for presentation

--- Program.java
+++ Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main (String[] args) {
-        System.out.println("Hello World!")
+        System.out.println("Hello World!");
     }
 
     public static int AddInt(int a, int b) {
