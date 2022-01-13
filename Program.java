078a7dcc14129a1c38e3c7e19511b4ae33c28ce3
georgreichert <81485014+georgreichert@users.noreply.github.com>
2022-01-14 00:22:30 +0100

    Presentation commit

--- Program.java
+++ Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main (String[] args) {
-        System.out.println("This is a program to show the use of Jenkins.")
+        System.out.println("This is a program to show the use of Jenkins.");
     }
 
     public static int AddInt(int a, int b) {
