303f133543f3291437b3bca637789f4b3040f16a
georgreichert <81485014+georgreichert@users.noreply.github.com>
2022-01-14 09:54:18 +0100

    Presentation commit

--- Tests.java
+++ Tests.java
@@ -2,7 +2,7 @@ public class Tests {
     public static void main (String[] args) {
         assert Program.AddInt(1,2) == 3 : "Testing AddInt failed";
         assert Program.AddInt(5,6) == 11 : "Testing AddIntWrong failed";
-        //assert Program.SubtractInt(8,4) == 4 : "Testing SubtractInt failed";
+        assert Program.SubtractInt(8,4) == 4 : "Testing SubtractInt failed";
         //assert Program.SubtractInt(12,21) == -9 : "Testing SubtractInt failed";
     }
 }
